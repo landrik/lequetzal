4841c1c934f9ee7353bef06ec650526710f73c7a
Landry <landrik@gmail.com>
2017-02-17 15:02:55 +0000

    gitignore

--- bs-config.js
+++
@@ -1,9 +0,0 @@
-const proxyMiddleware = require('http-proxy-middleware');
-const proxyURL = 'http://localhost:9876';
-
-module.exports = {
-  ghostMode:false,
-  server: {
-    middleware: [proxyMiddleware('/api', { target: proxyURL })]
-  }
-}
